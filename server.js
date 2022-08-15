8bde8c9d581a609b49a26c8c282e20f37140d0ed
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2022-08-15 06:48:38 +0200

    connect ot MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
 const PORT = process.env.PORT || 8000;
 
+// Connect with DB
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log(`Database connected successfully`);
+  })
+  .catch((err) => {
+    console.log(`Database Error: ${err.message}`);
+  });
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
-  console.log(`mode: ${process.env.PORT}`);
+  console.log(`You are in Dev mode at port: ${process.env.PORT}`);
 }
 
 app.get("/", (req, res) => {
